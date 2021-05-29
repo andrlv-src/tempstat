f60513aaac1363ccd75baf6627bc11272c1f7f32
Andrey <0x81zu@gmail.com>
2021-05-29 21:42:54 +0300

    refactor: make some refactoring

--- main.c
+++ main.c
@@ -20,74 +20,77 @@
 #include <stdlib.h>
 #include "temp_functions.h"
 
+enum {
+        MAX_FILE_NAME_LEN = 100,
+        MONTHS = 12
+};
+
 int main(int argc, char *argv[])
 {
 
-        FILE *f;
+        FILE *fp = NULL;
         _Bool is_file_set = false;
-        char fname[100] = {1}; /* csv file, which will be read for dada */
+        char fname[MAX_FILE_NAME_LEN] = {}; /* file, which will be read for dada */
         int arg = 0;        
+        data_s months[MONTHS];
 
-        if (argc == 1) {
-                printf("ERROR: using without arguments.\n");
-                printf("Try tempstat -h for help.\n");
-                exit(1);
-        }
+        argcheck(argc);
         opterr = 0;
-
         while ((arg = getopt(argc, argv, "f:hm:vy:")) != -1) {
                 switch (arg) {
-                        case 'f':
-                                is_file_set = true;
-                                strcpy(fname, optarg);
-                                break;
-                        case 'h':
-                                print_help();
-                                return 0;
-                                break;
-                        case 'v':
-                                print_version();
-                                return 0;
-                                break;
-                        case 'm':
-                                /* TODO check for file set because all options
-                                 follow only when file is set */
-                                printf("Found arg \"m = %s\".\n", optarg);
-                                break;
-                        case 'y':
-                                printf("Found arg \"y = %s\".\n", optarg);
-                                break;
-                        case '?':
-                                printf("ERROR: Wrong argument(s).\n");
-                                printf("Try tempstat -h for help.\n");
-                                exit(1);
-                                break;
-                        default:
-                                printf("Error! Wrong argument! Default case exit.\n");
-                                exit(1);
-                                break;
+                case 'f':
+                        is_file_set = true;
+                        check_file_len(MAX_FILE_NAME_LEN, strlen(optarg));
+                        strcpy(fname, optarg);
+                        break;
+                case 'h':
+                        print_help();
+                        return 0;
+                        break;
+                case 'v':
+                        print_version();
+                        return 0;
+                        break;
+                case 'm':
+                        /* TODO check for file set because all options
+                           follow only when file is set */
+                        printf("Found arg \"m = %s\".\n", optarg);
+                        break;
+                case 'y':
+                        printf("Found arg \"y = %s\".\n", optarg);
+                        break;
+                case '?':
+                        printf("ERROR: Wrong argument(s).\n");
+                        printf("Try tempstat -h for help.\n");
+                        exit(1);
+                        break;
+                default:
+                        printf("Error! Wrong argument! Default case exit.\n");
+                        exit(1);
+                        break;
                 }
         }
 
         /* read file */
-        read_file(f, fname);
+        fp = fopen(fname, "r");
+        if (fp == NULL) {
+                printf("ERROR: file \"%s\" not found.\n", fname);
+                exit(1);
+        }
 
+        parse_data(fp, months);
 
-        /* data structure *inp_file, "r" */
+        fclose(fp);
 
         /* parse data , TODO check csv file errors, show line __LINE__
            processing through scanf
            scanf("%4d;%2d;%2d;%2d;%2d;%d", &in);
            there need to test different delimiter for additonal functionality
         */
-
-
         
         /* out/printing */
         
         /* additonal func */
-
-        /* test args */
 
         return 0;
 }
