dd77b4d7d7c3d624c8efa572c90d3449f33fdeea
Andrey <0x81zu@gmail.com>
2021-05-29 21:37:38 +0300

    feat: add new funct argcheck, check_file_len ...

--- temp_functions.h
+++ temp_functions.h
@@ -4,13 +4,24 @@
  *
  *      written 2021 by Andrey Lvov
  */
+
 #ifndef temp_functions_h
 #define temp_functions_h
 
 #define VERSION "0.1"
 
+typedef struct {
+        int year;
+        int month;
+        int tmax;
+        int tmin;
+        int avr_month_temp;
+} data_s;
+
 void print_help();
 void print_version();
-void read_file(FILE *f, char *fname);
+void check_file_len();
+void argcheck(int args);
+int parse_data(FILE *f, data_s *months);
 
 #endif
