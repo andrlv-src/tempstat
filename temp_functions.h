a62ef3b36f85ab6eb868c3922735d8af0a114fec
Andrey <0x81zu@gmail.com>
2021-05-29 13:20:56 +0300

    feat: add version variable

--- temp_functions.h
+++ temp_functions.h
@@ -1,9 +1,13 @@
 /*      temp_functions.h
  *
+ *      Difinition of tempstat functions.
+ *
  *      written 2021 by Andrey Lvov
  */
 #ifndef temp_functions_h
 #define temp_functions_h
+
+#define VERSION "0.1"
 
 void print_help();
 void print_version();
