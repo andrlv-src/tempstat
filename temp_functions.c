8bb39e840a216185b4e4ab96f6641785d7b11c0a
Andrey <0x81zu@gmail.com>
2021-05-29 15:21:09 +0300

    feat: add new help messages

--- temp_functions.c
+++ temp_functions.c
@@ -8,8 +8,36 @@
 
 void print_help()
 {
-        printf("Usage: tempstat [OPTION]... [FILE]...\n");
-        printf("  -f\tfile for processing\n");
-        printf("Examples:\n");
+/* 
+ *  options list
+ *
+ *  -f=filename.csv
+ *  -h help
+ *  -m=month number
+ *  -v version
+ *  -y=year
+ *  -D delimiter . , ; : "
+ *  -M default month
+ *
+ */
+        printf("Usage: tempstat -f [FILE]...[OPTIONS]\n");
+        // printf("  -D\tset specific argument for csv delimeter like , . " : ;\n");
+        printf("  -f\t[FILE] for processing in csv format\n");
+        printf("  -h\tShow this message\n");
+        printf("  -v\tShow version info\n");
+        printf("\n[OPTIONS]\n");
+        printf("  -m\tDisplay particular month by using nubmers from 1 to 12\n");
+        // you can choose delimeter by -D option
+        printf("\nExamples:\n");
         printf("  tempstat -f file.csv -m 1\n");
 }
+
+void print_version()
+{
+        printf("tempstat version %s\n", VERSION);
+        printf("  Copyright (C) 2021 Andrey Lvov\n");
+        // printf("This is free software: you are free to change and redistribute it.\n");
+        printf("  There is NO WARRANTY, to the extent permitted by law.\n");
+
+}
+
