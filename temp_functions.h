6656d51736024c41876701714c53d4ddb55b069d
Andrey <0x81zu@gmail.com>
2021-05-30 20:50:18 +0300

    feat: add new funcs definition dbinit and dbset

--- temp_functions.h
+++ temp_functions.h
@@ -10,20 +10,30 @@
 
 #define VERSION "0.1"
 
+enum {
+        MAX_FILE_NAME_LEN = 100,
+        NUMBER_OF_MONTHS = 12
+};
+
 typedef struct {
         int year;
         int month;
         int days;
         int tmax;
         int tmin;
-        int avr_month_temp;
+        int avr_t;   /* average month temperature */
+        int minutes; /* amount minutes correspond to temperature */
+        int tsum;    /* sum of all temper measures. tsum / minutes = avr_t */
 } data_s;
 
+void dbinit(data_s *tdata);
+void dbset();/*TODO*/
 void print_help();
 void print_version();
 void check_file_len();
 void argcheck(int args);
-void parse_data(int *data, data_s *months);
-int flread(FILE *f, data_s *months);
+int read_data(FILE *f, data_s *tdata);
+void parse_data(int *data, data_s *tdata);
+void print_data(data_s *tdata);
 
 #endif
